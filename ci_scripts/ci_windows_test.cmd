cd45b91a1530a68ce019b876a32b1694b4031613
Darrell Wright <beached@users.noreply.github.com>
2021-08-25 02:16:12 -0400

    Added basic CI workflow and install testing (#1)
    * Added basic CI workflow and install testing
    
    * Added missing info in config
    
    * Reformant of cmakelist
    
    * Missing find_package
    
    * Fixed install to account for dependencies
    
    * Using prefix paths on windows

---
+++ ci_scripts/ci_windows_test.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd build
+
+ECHO "#################################3"
+ECHO "Starting Tests"
+REM "C:/PROGRAM FILES (X86)/MICROSOFT VISUAL STUDIO/2019/ENTERPRISE/COMMON7/IDE/COMMONEXTENSIONS/MICROSOFT/CMAKE/CMake/bin/ctest.exe" -C Debug --target full -j 2 -VV --timeout 300
