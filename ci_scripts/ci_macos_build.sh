e1819b27ea30609c68496b26ed23c8dba603defb
Darrell Wright <beached@users.noreply.github.com>
2021-08-25 01:00:38 -0400

    Added basic CI workflow and install testing

---
+++ ci_scripts/ci_macos_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+echo "#################################"
+echo "Configure Cmake"
+cmake -DCMAKE_BUILD_TYPE=Debug .. -DDAW_ENABLE_TESTING=On
+
+echo "#################################"
+echo "Starting full build"
+cmake --build . --config Debug --target full -j 2 -- -k 1000
+echo "#################################"
+echo "Full build complete"
---
+++ ci_scripts/ci_macos_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd build
+
+echo "#################################"
+echo "Starting Tests"
+#ctest -C Debug --target full -j 2
